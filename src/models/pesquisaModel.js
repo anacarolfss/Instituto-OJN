4b90cc99a78110954c892f5025b12c8fd709356b
ana carol <ana.ssilva@sptech.school>
2026-05-23 22:05:57 -0300

    Mudança DashBoard/Textos-etc
    .

--- src/models/pesquisaModel.js
+++ src/models/pesquisaModel.js
@@ -1,17 +1,18 @@
 var database = require("../database/config");
 
 function cadastrar(problemaAbandono) {
-
+    console.log(">> MODEL PESQUISA - cadastrar");
+    console.log("Valor recebido:", problemaAbandono);
     var instrucaoSql = `
         INSERT INTO pesquisa (problemaAbandono)
         VALUES ('${problemaAbandono}');
     `;
-
+    console.log("Executando SQL INSERT pesquisa...");
     return database.executar(instrucaoSql);
 }
 
 function buscarDadosGrafico() {
-
+      console.log(">> MODEL PESQUISA - buscar gráfico");
     var instrucaoSql = `
         SELECT problemaAbandono,
         COUNT(problemaAbandono) AS quantidade
