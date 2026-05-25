f25a93005d6a4da24627dfb0c1c854f2e5a20b6c
ana carol <ana.ssilva@sptech.school>
2026-05-25 18:04:43 -0300

    Conexão VM- Banco de dados

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-require("dotenv").config(); //evita q os dados sensiveis fique expostos no código 
+var ambiente_processo = 'producao'
+// var ambiente_processo = 'desenvolvimento'
+
+var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+
+
+
+require("dotenv").config({path: caminho_env}); //evita q os dados sensiveis fique expostos no código 
 
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const PORTA = 3000;
-const pesquisaRouter = require("./src/routes/pesquisa");
+var PORTA_APP = process.env.APP_PORT;
+var HOST_APP = process.env.APP_HOST;
 
 const app = express();
+
+const pesquisaRouter = require("./src/routes/pesquisa");
 
 const usuarioRouter = require("./src/routes/usuarios");
 
@@ -19,7 +28,7 @@ app.use(cors());
 app.use("/usuarios", usuarioRouter);
 app.use("/pesquisas", pesquisaRouter);
 
-app.listen(PORTA, () => {
-  console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://localhost:${PORTA}`);
+app.listen(PORTA_APP, function () {
+  console.log(`Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar: http://${HOST_APP}:${PORTA_APP}`);
 });
 
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -3,7 +3,7 @@
     function autenticar(email, senha) {
         console.log("ACESSEI O USUARIO MODEL", email, senha)
         var instrucaoSql = `
-            SELECT id, nome, email, data_nascimento FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+            SELECT id, nome, email, data_nascimento FROM Usuario WHERE email = '${email}' AND senha = '${senha}';
         `;
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
         return database.executar(instrucaoSql);
@@ -13,7 +13,7 @@
         console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",  nome, email, dataNascimento, senha);
     
         var instrucaoSql = `
-            INSERT INTO usuario (nome, email, data_nascimento, senha) VALUES ('${nome}', '${email}', '${dataNascimento}', '${senha}');
+            INSERT INTO Usuario (nome, email, data_nascimento, senha) VALUES ('${nome}', '${email}', '${dataNascimento}', '${senha}');
         `;
         console.log("Executando a instrução SQL: \n" + instrucaoSql);
         return database.executar(instrucaoSql);
